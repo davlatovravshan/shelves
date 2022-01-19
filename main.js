d0d7f80d97efce9fca5d53f9c4be1a28de6efca7
Ravshan Davlatov <55549068+davlatovravshan@users.noreply.github.com>
2022-01-20 00:42:45 +0500

    Complete order block

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let menuBurger = document.querySelector('.menu-burger')
 menuBurger.addEventListener('click', function() {
     this.classList.toggle('change')
 })
+
+
+$('.wares-cards').magnificPopup({
+    type: 'image',
+    delegate: 'a',
+    gallery: {
+      enabled:true
+    }
+});
