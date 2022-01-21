8443fac8032b4025f26f4dbeb57422d7212c3656
Ravshan Davlatov <55549068+davlatovravshan@users.noreply.github.com>
2022-01-22 02:17:54 +0500

    Complete modal and adaptive menu

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 let menuBurger = document.querySelector('.menu-burger')
+let modalBtns = document.querySelectorAll('[data-modal]')
+let menu = document.querySelector('#menu')
+let modal = document.querySelector('#modal')
+let modalWrapper = document.querySelector('.modal_wrapper')
 
 menuBurger.addEventListener('click', function() {
     this.classList.toggle('change')
+    menu.classList.toggle('active')
 })
 
+
+$('[data-modal]').click(function() {
+	$(modal).addClass('active')
+})
+
+$(modal).click(function() {
+	$(modal).removeClass('active')
+})
+
+$(modalWrapper).click(function(event) {
+	event.stopPropagation()
+})
 
 $('.wares-cards').magnificPopup({
     type: 'image',
